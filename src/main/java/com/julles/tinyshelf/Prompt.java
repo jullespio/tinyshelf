6c2875199334806d2b1efac32b479868c8b8f02e
Julles <juliokirk@gmail.com>
2024-10-21 15:58:11 -0300

    Abstract class Prompt updated

--- src/main/java/com/julles/tinyshelf/Prompt.java
+++ src/main/java/com/julles/tinyshelf/Prompt.java
@@ -1,5 +1,50 @@
 package com.julles.tinyshelf;
 
+import java.util.Scanner;
+
 abstract class Prompt {
+
+    Scanner scanner;
+
+    Prompt(){
+
+        scanner = new Scanner(System.in);
+
+    }
+
+    public void displayGreetings(){
+
+        System.out.println("\nGreetings!\n>>>> Welcome to tinyShelf <<<<");
+        
+    }
+
+    Boolean continueOrNot(){
+        while (true) {
+            
+            System.out.println("\nContinue? [y/n]");
+            System.out.print("> ");
+            String answer = scanner.nextLine();
+            System.out.println();
+            
+            if (answer.equalsIgnoreCase("y")) {
+                return true;
+            } 
+            
+            if (answer.equalsIgnoreCase("n")) {
+                return false;
+            } 
+
+            if (!(answer.equalsIgnoreCase("y") || answer.equalsIgnoreCase("n"))) {
+                System.out.println("\nPlease provide a valid answer.\n");
+                continue;
+            }
+
+            break;
+
+        }
+        
+        return false;
+    }
+
 
 }
